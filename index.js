bad9b7736430127a3db9cba259d63ee34519e122
Jonathan Boulanger <Jonathan_Boulanger@Hotmail.com>
2016-07-17 14:28:52 -0400

    Changed api in order to allow for an optional options object. This means that the logger object must be instantiated

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-var logger = function(req, res, next) {
-	console.log('Request: {' + req.method + '} ' + req.url);
-	next();
+function loggerFactory(options) {
+	var loggingFunction = (options && options.logger) ? options.logger : console.log.bind(console);
+
+	return function logger(req, res, next) {
+		logMainStatement(req, loggingFunction);
+    if (options && options.extraFields)
+    	logExtraFields(req, loggingFunction, options.extraFields);
+		next();
+	}
 }
 
-module.exports = logger; 
+function logMainStatement(req, loggingFunction) {
+	loggingFunction('Request: {' + req.method + '} ' + req.url);
+}
+
+function logExtraFields(req, loggingFunction, extraFields) {
+	extraFields.map(function(fieldName) {
+  	if (req[fieldName]) {
+      var field = typeof req[fieldName] == 'object' ? JSON.stringify(req[fieldName]) : req[fieldName];
+    }
+    	console.log(fieldName + ': ' + field);
+  });
+}
+
+module.exports = loggerFactory;
